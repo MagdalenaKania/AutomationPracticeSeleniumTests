c8f533a42a14f54b2ed4c907631f116a83cc4d1a
MagdalenaKania <magdalenakania1995@gmail.com>
2021-02-17 13:02:12 +0100

    moving test attachment to resources

--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -1,6 +1,11 @@
 import org.junit.jupiter.api.Test;
 import pages.HomePage;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContactTest extends BaseTest {
@@ -8,13 +13,16 @@ public class ContactTest extends BaseTest {
     HomePage homePage = new HomePage();
 
     @Test
-    public void asLoginUserIShallSentFormWithAttachment() {
+    public void asLoginUserIShallSentFormWithAttachment() throws URISyntaxException {
+        URL filePath = getClass().getClassLoader().getResource("test.txt");
+        File file = Paths.get(filePath.toURI()).toFile();
+        String absolutePath = file.getAbsolutePath();
         boolean isMessageSent = homePage.clickOnContactUs()
                 .selectSubjectHeading("Customer service")
                 .inputEmailAddress("test@gmail.com")
                 .inputOrderId("12345")
                 .inputContactMessage("Test")
-                .attachingFile("C:\\\\Users\\\\mkania\\\\OneDrive - Objectivity Sp. z o.o\\\\Pulpit\\\\test.txt")
+                .attachingFile(absolutePath)
                 .clickOnSendButton()
                 .isMessageSent();
 
